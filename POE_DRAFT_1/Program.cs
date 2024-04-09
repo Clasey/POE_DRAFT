eec0b8f521223e03e360799d599ba4e3b0642227
Clasey <Claseyk@gmail.com>
2024-04-09 12:47:06 +0200

    Declaration

---
+++ POE_DRAFT_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE_DRAFT_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaration of types of data
+            public string name;
+            public string quantity;
+            public string unit;
+        }
+       
+    }
+}
