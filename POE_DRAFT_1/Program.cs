130ae3734b4f6580c18e712f2bbfbfff5ae8af69
Clasey <Claseyk@gmail.com>
2024-04-09 12:49:22 +0200

    correction for error

--- POE_DRAFT_1/Program.cs
+++ POE_DRAFT_1/Program.cs
@@ -8,13 +8,9 @@ namespace POE_DRAFT_1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void main(string [] args)
         {
-            // Declaration of types of data
-            public string name;
-            public string quantity;
-            public string unit;
+
         }
-       
     }
 }
